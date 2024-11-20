7f801353b2348d3129b276d321e31a86bc53d892
Joel AZEMAR <joel.azemar@gmail.com>
2024-11-20 14:55:38 +0100

    Update cypress.config.js

--- system/cypress/cypress.config.js
+++ system/cypress/cypress.config.js
@@ -3,7 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3025",
-    pageLoadTimeout: 120000, // Increase to 2 minutes
+    // pageLoadTimeout: 120000, // Increase to 2 minutes
+    pageLoadTimeout: 300000, // 5 minutes
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
